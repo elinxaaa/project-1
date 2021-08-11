bc0e5411bafe481fb1fe37ce98c8618aa0d05e42
Elina Zheng <47897713+elinxaaa@users.noreply.github.com>
2021-08-10 20:02:33 -0400

    added rgb randomization

--- turtle graphics/random-walk.py
+++ turtle graphics/random-walk.py
@@ -1,16 +1,26 @@
-from turtle import Turtle, Screen
-from random import choice
+import turtle as t
+from random import choice, randint
 
-toe = Turtle()
-turtle_colours = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray"]
+t.colormode(255)
+toe = t.Turtle()
+
+
+def rand_colour():
+    red = randint(0, 255)
+    green = randint(0, 255)
+    blue = randint (0, 255)
+    rgb = (red, green, blue)
+    return rgb
+
+
 toe.pensize(10)
 toe.speed(10)
 direction = [0, 2, 3, 1]
 
 for _ in range(100):
-    toe.color(choice(turtle_colours))
+    toe.color(rand_colour())
     toe.forward(20)
     toe.setheading(90*choice(direction))
 
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
