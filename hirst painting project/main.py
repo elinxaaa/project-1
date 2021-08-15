4bba37679d8b3f78414f04198edb8373cb86c65c
Elina Zheng <47897713+elinxaaa@users.noreply.github.com>
2021-08-14 20:42:02 -0400

    added colour extractor

---
+++ hirst painting project/main.py
@@ -0,0 +1,12 @@
+import colorgram
+
+colours = colorgram.extract('image.jpg', 30)
+rgb_colours = []
+for colour in colours:
+    r = colour.rgb.r
+    g = colour.rgb.g
+    b = colour.rgb.b
+    new_colour = (r, g, b)
+    rgb_colours.append(new_colour)
+
+print(rgb_colours)
