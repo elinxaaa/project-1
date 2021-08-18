eca5effdbb60da3a88ccca2827fb91ef59b06e90
Elina Zheng <47897713+elinxaaa@users.noreply.github.com>
2021-08-18 08:03:07 -0400

    multiple turtle instances

---
+++ project 2/turtle_race.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+red = Turtle()
+red.color("red")
+orange = Turtle()
+orange.color("orange")
+yellow = Turtle()
+yellow.color("yellow")
+green = Turtle()
+green.color("green")
+blue = Turtle()
+blue.color("blue")
+
+
+
+
+screen = Screen()
+screen.exitonclick()
