0fb735933ebd1f9e79a4fd89f1a18a1f6a7c2481
Elina Zheng <47897713+elinxaaa@users.noreply.github.com>
2021-09-07 22:51:49 -0400

    added food and food collision detection

--- snake game/main.py
+++ snake game/main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,22 +10,26 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 game_run = True
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.right, "d")
+screen.onkey(snake.left, "a")
 
 while game_run:
     screen.update()
     time.sleep(0.1)
-    screen.listen()
-    screen.onkey(snake.up, "Up")
-    screen.onkey(snake.down, "Down")
-    screen.onkey(snake.right, "Right")
-    screen.onkey(snake.left, "Left")
-    screen.onkey(snake.up, "w")
-    screen.onkey(snake.down, "s")
-    screen.onkey(snake.right, "d")
-    screen.onkey(snake.left, "a")
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        print("nom")
 
 screen.exitonclick()
